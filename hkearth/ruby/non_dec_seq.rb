0b77e0bf06184e68c4228c070a9b74d5b3604bb7
ashish91 <thecuriouslearner13@gmail.com>
2016-03-12 20:50:34 +0530

    Formatting

--- hkearth/ruby/non_dec_seq.rb
+++ hkearth/ruby/non_dec_seq.rb
@@ -7,23 +7,23 @@ def binary_search(arr, low, high, val)
 			high = mid
 		end
 	end
-	
+
 	return low
 end
- 
+
 testcases = gets.chomp.to_i
- 
+
 testcases.times do
 	n = gets.chomp.to_i
 	arr = gets.split(' ').slice(0, n).map(&:to_i)
 	l = gets.chomp.to_i
-	
+
 	a = Array.new(n, 0)
 	a[0] = arr[0]
-	
+
 	tail = 1
 	answer = []
-	
+
 	for i in (1..n-1)
 		if arr[i] < a[0]
 			a[0] = arr[i]
@@ -34,12 +34,12 @@ testcases.times do
 			place = binary_search(a, 0, tail - 1, arr[i])
 			a[place] = arr[i]
 		end
-		
+
 		if(tail >= l)
 			answer.push(a[l-1])
 		end
 	end
- 
+
 	if answer.empty?
 		puts "-1"
 	else
